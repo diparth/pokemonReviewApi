05c19db538032251a81a54a41573214dec6d31e8
Diparth Patel <pateldiparth309@gmail.com>
2023-08-28 09:50:23 -0400

    data and context added

---
+++ Data/DataContext.cs
@@ -0,0 +1,22 @@
+using Microsoft.EntityFrameworkCore;
+using pokemonReviewApp.Models;
+
+namespace pokemonReviewApp.Data
+{
+  public class DataContext : DbContext
+  {
+    public DataContext(DbContextOptions<DataContext> options) : base(options)
+    {
+
+    }
+    
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Country> Countries { get; set; }
+    public DbSet<Owner> Owners { get; set; }
+    public DbSet<Pokemon> Pokemon { get; set; }
+    public DbSet<PokemonOwner> PokemonOwners { get; set; }
+    public DbSet<PokemonCategory> PokemonCategories { get; set; }
+    public DbSet<Review> Reviews { get; set; }
+    public DbSet<Reviewer> Reviewers { get; set; }
+  }
+}
